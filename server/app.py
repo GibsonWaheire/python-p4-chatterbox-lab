b00269baabb7d62424726334aba8c34e3be660a9
Gibson Waheire Giteru <pwriter455@gmail.com>
2025-09-19 11:39:22 +0300

    Complete Chatterbox Lab - Flask API with CRUD routes and proper migrations

--- server/app.py
+++ server/app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
 
-# Import database models
 from models import db, Message
 
 app = Flask(__name__)
